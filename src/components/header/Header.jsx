b9df7945b1484b993f98aadd53228a1fd1e4ea1d
dkyzym <kyzym12345@gmail.com>
2025-01-12 10:46:56 +0100

    feat: logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,51 @@
 import { SocketStatusIndicator } from '@components/indicators/SocketStatusIndicator';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Button,
+  SvgIcon,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
+import { default as logoUrl } from '../../images/logo.svg';
+
+const LogoIcon = (props) => (
+  <SvgIcon {...props}>
+    <image href={logoUrl} width="100%" height="100%" color="white" />
+  </SvgIcon>
+);
 
 export const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar sx={{ width: '100%' }}>
-        <Typography variant="h6" component={Link} to="/" sx={{ flex: 1 }}>
-          Шикарное название
-        </Typography>
+        <Box
+          component={Link}
+          to="/"
+          sx={{
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+          }}
+        >
+          <Box sx={{ maxWidth: '200px' }}>
+            <Box
+              sx={{
+                height: '50px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <LogoIcon sx={{ fontSize: '120px' }} />
+            </Box>
+          </Box>
+          <Typography variant="h6" sx={{ fontSize: 12, fontWeight: 100 }}>
+            Шикарное название
+          </Typography>
+        </Box>
 
         <SocketStatusIndicator />
         <Button
