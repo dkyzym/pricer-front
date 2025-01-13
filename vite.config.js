544a0636c3ebbdae601ad8527133106ab29676ff
dkyzym <kyzym12345@gmail.com>
2025-01-13 08:55:35 +0100

    feat: paths

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
