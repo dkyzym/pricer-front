4d35ac558f4ad8163e08a00a816433014afae9b5
dkyzym <kyzym12345@gmail.com>
2025-03-01 08:28:55 +0100

    refactor: removed tooltip

--- src/components/indicators/SupplierStatusIndicator.jsx
+++ src/components/indicators/SupplierStatusIndicator.jsx
@@ -27,35 +27,33 @@ export const SupplierStatusIndicator = ({
   onChange,
 }) => {
   return (
-    <Tooltip title="Выберите поставщика">
-      <Box>
-        <FormGroup>
-          <Stack direction="row" alignItems="center" spacing={1}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checked}
-                  onChange={() => onChange(supplierKey)}
-                  color="primary"
-                />
-              }
-              label={supplierNameMap[supplierKey]}
-            />
+    <Box>
+      <FormGroup>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={() => onChange(supplierKey)}
+                color="primary"
+              />
+            }
+            label={supplierNameMap[supplierKey]}
+          />
 
-            <Box sx={{ width: 20, display: 'flex', justifyContent: 'center' }}>
-              {status.loading && <CircularProgress size={16} />}
-              {!status.loading && status.error && (
-                <Tooltip title={status.error}>
-                  <ErrorOutlineIcon color="error" />
-                </Tooltip>
-              )}
-              {!status.loading && !status.error && (
-                <Typography>{status.results.data?.length}</Typography>
-              )}
-            </Box>
-          </Stack>
-        </FormGroup>
-      </Box>
-    </Tooltip>
+          <Box sx={{ width: 20, display: 'flex', justifyContent: 'center' }}>
+            {status.loading && <CircularProgress size={16} />}
+            {!status.loading && status.error && (
+              <Tooltip title={status.error}>
+                <ErrorOutlineIcon color="error" />
+              </Tooltip>
+            )}
+            {!status.loading && !status.error && (
+              <Typography>{status.results.data?.length}</Typography>
+            )}
+          </Box>
+        </Stack>
+      </FormGroup>
+    </Box>
   );
 };
