519c2347e1ddcf67915b5873e1e8ae2d9c1b7fa8
dkyzym <kyzym12345@gmail.com>
2025-03-05 14:21:51 +0300

    fix: hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,7 +45,7 @@ export const Header = () => {
               </Box>
             </Box>
             <Typography variant="h6" sx={{ fontSize: 12, fontWeight: 100 }}>
-              Супер Шикарное название
+              Шикарное Название
             </Typography>
           </Box>
 
